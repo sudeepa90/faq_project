0f0d8afda3e33b2fc34610f6e5e8ea00ec09438b
sudeepa90 <107499030+sudeepa90@users.noreply.github.com>
2025-02-01 23:08:19 +0530

    Create models.py

---
+++ faqs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True)
+    answer_hi = RichTextField(blank=True)
+    question_bn = models.TextField(blank=True)
+    answer_bn = RichTextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
