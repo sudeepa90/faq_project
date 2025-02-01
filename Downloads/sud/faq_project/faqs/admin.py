93ddce1ba094d21079fb260d4ba1618ea30d10c4
sudeepa90 <107499030+sudeepa90@users.noreply.github.com>
2025-02-01 22:48:49 +0530

    Delete Downloads directory

--- Downloads/sud/faq_project/faqs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FAQ
-
-@admin.register(FAQ)
-class FAQAdmin(admin.ModelAdmin):
-    list_display = ('question', 'created_at', 'updated_at')
-    search_fields = ('question', 'answer')
-    readonly_fields = ('created_at', 'updated_at')
